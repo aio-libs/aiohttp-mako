d7dcfd7705f2464b9658c37c4cdbd9376e8063c7
Nickolai Novik <is.infinity@yahoo.com>
2015-04-10 00:14:50 +0300

    rework and remove anotaion

--- aiohttp_mako/__init__.py
+++ aiohttp_mako/__init__.py
@@ -1,15 +1,14 @@
 import asyncio
 import functools
-from mako.exceptions import html_error_template
+from collections import Mapping
+
 from aiohttp import web
 from mako.lookup import TemplateLookup
-from functools import partial
-
+from mako.exceptions import TemplateLookupException
 
 __version__ = '0.0.1'
-
 __all__ = ('setup', 'get_lookup', 'render_template', 'template',
-           'render_annotation')
+           'render_string')
 
 
 APP_KEY = 'aiohttp_mako_lookup'
@@ -24,9 +23,9 @@ def get_lookup(app, *, app_key=APP_KEY):
     return app.get(app_key)
 
 
-def _render_template(template_name, request, response, context, *,
-                     app_key, encoding):
+def render_string(template_name, request, context, *, app_key):
     lookup = request.app.get(app_key)
+
     if lookup is None:
         raise web.HTTPInternalServerError(
             text=("Template engine is not initialized, "
@@ -34,22 +33,23 @@ def _render_template(template_name, request, response, context, *,
                   "".format(app_key)))
     try:
         template = lookup.get_template(template_name)
-        body = template.render(**context)
-    except:
-        if request.app.get('DEBUG_TEMPLATE'):
-            error_body = html_error_template().render()
-            raise web.HTTPInternalServerError(body=error_body)
-        raise
-    response.content_type = 'text/html'
-    response.charset = encoding
-    response.body = body
+    except TemplateLookupException:
+        raise web.HTTPInternalServerError(
+            text="Template '{}' not found".format(template_name))
+    if not isinstance(context, Mapping):
+        raise web.HTTPInternalServerError(
+            text="context should be mapping, not {}".format(type(context)))
+    text = template.render_unicode(**context)
+    return text
 
 
 def render_template(template_name, request, context, *,
                     app_key=APP_KEY, encoding='utf-8'):
     response = web.Response()
-    _render_template(template_name, request, response, context,
-                     app_key=app_key, encoding=encoding)
+    text = render_string(template_name, request, context, app_key=app_key)
+    response.content_type = 'text/html'
+    response.charset = encoding
+    response.text = text
     return response
 
 
@@ -63,39 +63,11 @@ def template(template_name, *, app_key=APP_KEY, encoding='utf-8', status=200):
                 coro = func
             else:
                 coro = asyncio.coroutine(func)
-            response = web.Response()
             context = yield from coro(*args)
             request = args[-1]
-            _render_template(template_name, request, response, context,
-                             app_key=app_key, encoding=encoding)
+            response = render_template(template_name, request, context,
+                                       app_key=app_key, encoding=encoding)
             response.set_status(status)
             return response
         return wrapped
     return wrapper
-
-
-def render_annotation(func):
-
-    @asyncio.coroutine
-    @functools.wraps(func)
-    def wrapped(*args):
-        if asyncio.iscoroutinefunction(func):
-            coro = func
-        else:
-            coro = asyncio.coroutine(func)
-        annotations = func.__annotations__
-        if not annotations.get('return'):
-            return (yield from coro)
-
-        render = annotations.get('return')
-
-        context = yield from coro(*args)
-        request = args[-1]
-
-        response = render(request, context)
-        return response
-    return wrapped
-
-
-def T(template_name):
-    return partial(render_template, template_name)
