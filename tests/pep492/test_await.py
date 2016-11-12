81d2e9a120a0d00b9aa716842c8662b6560df230
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-11-12 18:29:08 +0200

    Fix tests

--- tests/pep492/test_await.py
+++ tests/pep492/test_await.py
@@ -1,19 +1,17 @@
-import aiohttp
 import aiohttp_mako
 
 
-def test_func(loop, create_server):
+async def test_func(app, test_client):
 
     @aiohttp_mako.template('tplt.html')
     async def func(request):
         return {'head': 'HEAD', 'text': 'text'}
 
-    async def go():
-        app, url = await create_server()
-        app.router.add_route('GET', '/', func)
+    app.router.add_route('GET', '/', func)
 
-        resp = await aiohttp.request('GET', url, loop=loop)
-        assert 200 == resp.status
-        txt = await resp.text()
-        assert '<html><body><h1>HEAD</h1>text</body></html>' == txt
-    loop.run_until_complete(go())
+    client = await test_client(app)
+
+    resp = await client.get('/')
+    assert 200 == resp.status
+    txt = await resp.text()
+    assert '<html><body><h1>HEAD</h1>text</body></html>' == txt
