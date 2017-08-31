cd4427b5627e61873fab860315cc1292c8b28ec8
Nikolay Novik <nickolainovik@gmail.com>
2017-08-30 23:27:41 -0400

    Clean examples and README (#66)
    * fix example
    
    * drop 3.4 use 3.6
    
    * update README

--- examples/simple.py
+++ examples/simple.py
@@ -16,16 +16,8 @@ async def init(loop):
     template = """<html><body><h1>${head}</h1>${text}</body></html>"""
     lookup.put_string('index.html', template)
     app.router.add_route('GET', '/', func)
-
-    handler = app.make_handler()
-    srv = await loop.create_server(handler, '127.0.0.1', 8080)
-    print("Server started at http://127.0.0.1:8080")
-    return srv, handler
-
+    return app
 
 loop = asyncio.get_event_loop()
-srv, handler = loop.run_until_complete(init(loop))
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    loop.run_until_complete(handler.finish_connections())
+app = loop.run_until_complete(init(loop))
+web.run_app(app, host='127.0.0.1', port=9000)
